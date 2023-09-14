b51a903867710bbba9a9600fd90e975d1422eca0
Ahmed Helal <ahmed.attia.helal@gmail.com>
2023-09-14 18:01:42 +0300

    initializing repo

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Lab1-Ansible
+
+### SSH Access
+
+![ssh_access.png](screenshots/ssh_access.png)
+
+
+### Verify ansible installation 
+
+
+![ansible_version.png](screenshots/ansible_version.png)
+
+
+
+
+### Verify inventory file
+
+![inventory_ping.png](screenshots/inventory_ping.png)
+
+
+
---
+++ aws_user_data.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Adding the New User Ansible with encrypted pass
+sudo useradd  -p'$1$ekykTdPh$oqYQ/gokTzA11HDFVsK1I/' -m  -s /usr/bin/bash ansible 
+
+# giving the user sudo previlage
+sudo usermod ansible -aG sudo
+
+# adding the authorized_key
+sudo -u ansible -- mkdir -p /home/ansible/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+l+R8f7tQRqo3IEr73zv7wqx3tRQhLPNpQsjC+aSVFp6+fk9XFxrJKmfJwLN9sdQ46AYrO2LtgUdXZ3rmffNGBYeDC76Is3Per451i/IVUAxzltZz0aBZ5UEfyYleSONUBaWEHXSk19uRGwLKdl6KXFHGHaw3CaA+TJZPxPZ+fMQbKqNgDMpnsq2VS+SJne2nmdWHR4Wqz2dfo/pFmsD+Yoxo9CbghrQ2vG0vXuiz5k64us8Dw8akLQ9R8Bz4RywuCi+xt64SuDmw+LigaqruZpnhqq0JjkuR2nR4i8wXPeY6/xLZbAwjPzMXwyQ6g7lKTDROoCt2c9LEvHYo80I8G1DCkyKwoy8FJtWDn6M6WPIeOQDuq72lg42FKwlkjn/bV7IGmM5GZ1bE9Gha1vMVBgMzNkeAZtetrHXdO3BxQWLTvxP7L+a+wztf5gHQ4hm6zdNpHECXGidKZitYtTMJR45dJKbXUCKNmnggmZ8iVnWDXxuI+/5gG4UO0dorjMs= attia@rocky.dance.party' \
+ | sudo -u ansible -- tee  -a  /home/ansible/.ssh/authorized_keys
+
+# Enabling password logging through ssh (disabled by default)
+echo 'PasswordAuthentication yes'|sudo  tee -a  /etc/ssh/sshd_config.d/passwrd_allow.conf
+sudo service sshd restart
